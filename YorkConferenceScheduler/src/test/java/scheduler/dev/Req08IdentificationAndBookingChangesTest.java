0d8a15f26d41d2ee05e6d3343b5745d3ee86f333
Ahmed-Anique-215515554 <46542823+Ahmed-Anique-215515554@users.noreply.github.com>
2026-08-03 19:08:52 -0400

    Added test cases

--- YorkConferenceScheduler/src/test/java/scheduler/dev/Req08IdentificationAndBookingChangesTest.java
+++ YorkConferenceScheduler/src/test/java/scheduler/dev/Req08IdentificationAndBookingChangesTest.java
@@ -3,6 +3,9 @@ package scheduler.dev;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import java.time.LocalDateTime;
 import scheduler.booking.*;
 import scheduler.user.*;
@@ -18,6 +21,7 @@ public class Req08IdentificationAndBookingChangesTest {
 	private LocalDateTime end;
 	private PaymentStrategy testPayment;
 	private PricingStrategy testPricing;
+	private Map<String, Object> details;
 	
 	@Before
 	public void setUp() {
@@ -27,6 +31,7 @@ public class Req08IdentificationAndBookingChangesTest {
 		end = LocalDateTime.of(2026, 9, 10, 12, 0);
 		testPayment = new CreditCardPayment();
 		testPricing = new StudentPricing();
+		details = new HashMap<>();
 	}
 	
 	@Test
@@ -41,6 +46,33 @@ public class Req08IdentificationAndBookingChangesTest {
 		Booking booking = new Booking(testUser, testRoom, start, end, testPricing, testPayment);
 		booking.cancel();
 		assertThrows(IllegalStateException.class, () -> booking.modifyTimes(start.plusHours(1), end.plusHours(1)));
+	}
+	
+	@Test
+	public void testModifyBooking() {
+		Booking booking = new Booking(testUser, testRoom, start, end, testPricing, testPayment);
+		booking.modifyTimes(start.plusHours(1), end.plusHours(1));
+		assertEquals(start.plusHours(1), booking.getStartTime());
+		assertEquals(end.plusHours(1), booking.getEndTime());
+	}
+	
+	@Test
+	public void testUserValidation() {
+		String userID = "U006";
+		String email = "Dan2@yorku.ca";
+		String pwd = "Password!1";
+		String orgID = "951756";
+		details.put("userID", userID);
+		details.put("email", email);
+		details.put("password", pwd);
+		details.put("orgID", orgID);
+		
+		UserFactory factory = new UserFactory();
+		factory.createUser("student", details);
+		User user2 = factory.getUserByID(userID);
+		
+		assertTrue(user2.isVerified());
+		assertEquals(orgID, user2.getOrgID());
 	}
 	
 }
